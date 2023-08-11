f1042f8d73e72c583818ebbc5f13520f86f8bd3b
Amey <74102654+AmeyVijeesh@users.noreply.github.com>
2023-08-11 21:27:13 +0530

    Update generateSitemap.js

--- generateSitemap.js
+++ generateSitemap.js
@@ -1,9 +1,8 @@
 import fs from "fs";
 import { SitemapStream, streamToPromise } from "sitemap";
 
-const hostname = "https://www.yourwebsite.com"; // Replace with your actual website URL
+const hostname = "https://www.ameyvijeesh.netlify.com";
 
-// Define your website's routes
 const routes = [
   { url: "/", changefreq: "weekly", priority: 1.0 },
   { url: "/about", changefreq: "monthly", priority: 0.8 },
@@ -12,20 +11,16 @@ const routes = [
 ];
 
 (async () => {
-  // Create a sitemap instance
   const sitemap = new SitemapStream({ hostname });
 
-  // Add routes to the sitemap
   routes.forEach((route) => {
     sitemap.write(route);
   });
 
   sitemap.end();
 
-  // Generate the sitemap XML
   const xml = await streamToPromise(sitemap).then((data) => data.toString());
 
-  // Write the sitemap XML to a file
   fs.writeFileSync("./public/sitemap.xml", xml);
 
   console.log("Sitemap generated successfully.");
